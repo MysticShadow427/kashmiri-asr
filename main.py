906cc9eb957642a11f6a170782bf48c911ba411a
MysticShadow427 <devendrakayande427@gmail.com>
2024-07-28 16:41:48 +0530

    added main.py with config

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-import logging
+import sys
+import yaml
+import types
+
+if __name__ == "__main__":
+    cfg_yaml = sys.argv[1]
+    with open(cfg_yaml, 'r') as file:
+        dct = yaml.safe_load(file)
+        cfg = types.SimpleNamespace(**dct)
+
+    print(cfg)
